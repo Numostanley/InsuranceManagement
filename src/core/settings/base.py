227e8b5435b86a03eae085d00fbd5c4b97b95d6d
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-11-11 08:01:16 +0100

    refactor

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.companies",
     "apps.insurances",
     "apps.reviews",
     "apps.users"
