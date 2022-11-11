8c4842ac324b8336ac0d325c732d158e10c01010
Abdulrazaq <olalekanolanite@gmail.com>
2022-11-11 09:15:33 +0100

    Added user models

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=254, unique=True)
+    username = models.CharField(
+        max_length=254, null=True, blank=True, unique=True
+    )
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    phone_number = models.CharField(max_length=254, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=255, null=True)
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
